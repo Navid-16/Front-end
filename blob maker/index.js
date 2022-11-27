b769293d3a30dede75a29cc03762df55706b62e3
Navid_16 <77239915+Navid-16@users.noreply.github.com>
2022-11-27 09:48:34 +0200

    Update index.js

--- blob maker/index.js
+++ blob maker/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Project from "./instaProject/project"
+import Project from "./project1/project"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
